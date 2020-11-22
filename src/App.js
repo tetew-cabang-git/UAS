00093c88c24bf94301cb8bfe2960d9e9d3e33ac8
faisalapt <faisal.aprianto@student.umn.ac.id>
2020-11-22 17:26:42 +0700

    udh tampil nama negara

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Axios from "axios";
+import "./style.css";
+
+
+export default class App extends React.Component {
+  state = {
+    confirmed:0,
+    recovered:0,
+     deaths:0,
+    countries: []
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const resApi  = await Axios.get("https://covid19.mathdro.id/api/");
+    const resCountries  = await Axios.get("https://covid19.mathdro.id/api/countries");    
+    const count = resCountries.data.countries;
+    this.setState({
+      confirmed: resApi.data.confirmed.value,
+      recovered: resApi.data.recovered.value,
+      deaths: resApi.data.deaths.value,
+     countries: count
+    });
+  }
+
+  renderCountryOptions(){
+    return this.state.countries.map((country,i)=>{
+      return<option key={i}>{country.name}</option>
+    });
+  }
+
+  render() {
+    return ( 
+      <div className = "container" >
+        <h1>Corona Update</h1>
+        <select>
+          {this.renderCountryOptions()}
+        </select>
+        <div class="flex">
+          <div className="box confirmed">
+            <h3>Kasus dikonfirmasi</h3>
+            <h4> {this.state.confirmed} </h4>
+          </div>
+          <div className="box recovered">
+            <h3>Kasus Recovered</h3>
+            <h4> {this.state.recovered} </h4>
+          </div>
+          <div className="box deaths">
+            <h3>Kasus Meninggal</h3>
+            <h4> {this.state.deaths} </h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
